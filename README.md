a1bf4a5e4afe325d9f3b81235c13169fd6484a17
Ambroarun83 <arunnatarajan2704@gmail.com>
2024-03-07 15:58:20 +0530

    Update Readme file
    readme file updated and SQL file added.
    Screenshorts added.

--- README.md
+++ README.md
@@ -1,24 +1,65 @@
-# ealsuite-project
+# Simple Admin Portal 
 
-Screens:
-    Login Page
-    Dashboard
-    Customer List
-    Customer Edit screen
-    Invoice List
-    Invoice Edit screen
+# Screens
 
-Instructions:
-    1.Setup basic requirements like installing localhost servers like xampp,wampp,etc.
-    2.Run sql file in phpMyadmin under 'admin_portal' database.
-    3.The sql file is located in 'src/db/admin_portal.sql'
-    4.Run the index.php file using following link (only for localhost) : https://localhost/ealsuite-project/
-    5.Login using following credentials:
-        Username: admin
-        Password: admin
-    6.From the Dashboard page, click on 'Customer' or 'Invoice' to go to the list page.
-    7.You can add new customer or invoice using '+ Create' button on the list.
-    8.Fields that are mentioned * after it's label name are mandatory.
-    9.After submittion, you can edit and update the details using 'Edit' icon in the list.
-    10.Logout option is available in top-right corner of the page.
+Login Page
 
+Dashboard
+
+Customer List
+
+Customer Edit screen
+
+Invoice List
+
+Invoice Edit screen
+
+# Instructions
+
+#### 1. Setup:
+
+&emsp;Install localhost servers like XAMPP, WAMP, etc.
+
+#### 2. Database Setup:
+
+&emsp;Run the SQL file in phpMyAdmin under the 'admin_portal' database. The SQL file is located in src/db/admin_portal.sql.
+
+#### 3. Access Application:
+
+&emsp;Run the index.php file using the following link (only for localhost): https://localhost/ealsuite-project/.
+
+#### 4. Login Credentials:
+
+&emsp;Username: admin
+
+&emsp;Password: admin
+
+#### 5. Navigation:
+
+&emsp;From the Dashboard page, click on 'Customer' or 'Invoice' to go to the list page.
+
+#### 6. Add New Entries:
+
+&emsp;You can add a new customer or invoice using the '+ Create' button on the list.
+
+#### 7. Mandatory Fields:
+
+&emsp;Fields marked with * after the label name are mandatory.
+
+#### 8. Edit and Update:
+
+&emsp;After submission, you can edit and update the details using the 'Edit' icon in the list.
+
+#### 9. Logout:
+
+&emsp;Logout option is available in the top-right corner of the page.
+
+# Screenshot
+
+![Image of Login Page](https://github.com/Ambroarun83/ealsuite-project/blob/main/screenshots/add-customer.png)
+![Image of Dashboard](https://github.com/Ambroarun83/ealsuite-project/blob/main/screenshots/dashboard-page.png)
+![Image of Customer List](https://github.com/Ambroarun83/ealsuite-project/blob/main/screenshots/customer-list.png)
+![Image of Add Customer](https://github.com/Ambroarun83/ealsuite-project/blob/main/screenshots/add-customer.png)
+![Image of Invoice List](https://github.com/Ambroarun83/ealsuite-project/blob/main/screenshots/invoice-list.png)
+![Image of Add Invoice](https://github.com/Ambroarun83/ealsuite-project/blob/main/screenshots/add-invoice.png)
+![Image of Updating Detail](https://github.com/Ambroarun83/ealsuite-project/blob/main/screenshots/update.png)
---
+++ src/db/admin_portal.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 07, 2024 at 10:58 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `admin_portal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `cus_id` varchar(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice`
+--
+
+CREATE TABLE `invoice` (
+  `id` int(11) NOT NULL,
+  `inv_id` varchar(50) NOT NULL,
+  `cus_table_id` int(100) NOT NULL,
+  `date` date DEFAULT NULL,
+  `amount` varchar(255) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `invoice`
+--
+ALTER TABLE `invoice`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `customer table id` (`cus_table_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `invoice`
+--
+ALTER TABLE `invoice`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `invoice`
+--
+ALTER TABLE `invoice`
+  ADD CONSTRAINT `customer table id` FOREIGN KEY (`cus_table_id`) REFERENCES `customer` (`id`) ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
